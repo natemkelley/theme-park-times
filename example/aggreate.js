da0ceb8b37bb2fcb422b345380e51f5e786ff893
Nate Kelley <31867265+natemkelley@users.noreply.github.com>
2019-03-18 10:56:12 -0600

    Create aggreate.js

---
+++ example/aggreate.js
@@ -0,0 +1,59 @@
+//looking for a unique day
+[
+    {
+        '$match': {
+            'name': {
+                '$regex': 'Haunt'
+            }
+        }
+  }, {
+        '$group': {
+            '_id': {
+                'month': {
+                    '$month': '$lastUpdate'
+                },
+                'day': {
+                    '$dayOfMonth': '$lastUpdate'
+                },
+                'year': {
+                    '$year': '$lastUpdate'
+                },
+                'hour': {
+                    '$hour': '$lastUpdate'
+                }
+            },
+            'average': {
+                '$avg': '$waitTime'
+            }
+        }
+  }
+]
+
+//scefic day
+[
+  {
+    '$match': {
+      'name': {
+        '$regex': 'Haunt'
+      }, 
+      'lastUpdate': {
+        '$lte': Date('Mon, 18 Mar 2019 06:00:00 GMT'), 
+        '$gte': Date('Sun, 17 Mar 2019 06:00:00 GMT')
+      }
+    }
+  }, {
+    '$group': {
+      '_id': null, 
+      'average': {
+        '$avg': '$waitTime'
+      }, 
+      'max': {
+        '$max': '$waitTime'
+      }, 
+      'min': {
+        '$min': '$waitTime'
+      }
+    }
+  }
+]
+
