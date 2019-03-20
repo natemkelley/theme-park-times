b568c088921e9a9ee8fbaab82547c62af12a24d4
Nate Kelley <31867265+natemkelley@users.noreply.github.com>
2019-03-20 11:02:44 -0600

    Update disneyParkRideInfoController.js

--- gatherParkData/controllers/disneyParkRideInfoController.js
+++ gatherParkData/controllers/disneyParkRideInfoController.js
@@ -33,7 +33,7 @@ exports.saveRideInformation = function (rideInformation) {
 exports.getRideIDByPark = function (rideName, park) {
     //rideName = /.*Pirate.*/
     //var newrideName = new RegExp(".*" + rideName + ".*");
-    var newrideName = new RegExp(rideName.substring(0, (rideName.length - 3)));
+    var newrideName = new RegExp(".*" + rideName.substring(0, (rideName.length - 3)) + ".*");
 
     rideTimeDay
         .find({
@@ -46,14 +46,14 @@ exports.getRideIDByPark = function (rideName, park) {
             if (docs.length > 1) {
                 console.log(colors.cyan(rideName.substring(0, (rideName.length - 3)) + "->" + park));
                 docs.forEach(function (data) {
-                    console.log(colors.red(data.name + "->" + park));
+                    console.log(colors.yellow(data.name + "->" + park));
                 })
             } else {
                 console.log(colors.green(rideName + "->" + park));
             }
 
-            if (!docs) {
-                console.log(colors.yellow(rideName + "->" + park));
+            if (docs.length==0) {
+                console.log(colors.red(rideName + "->" + park));
             }
 
             //console.log(docs)
